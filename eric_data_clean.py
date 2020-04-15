180fedc1be59831704e00a6e95b280460b9b5fe8
EricB10 <ericblander@gmail.com>
2020-04-15 14:50:18 -0400

    data clean updates

--- eric_data_clean.py
+++ eric_data_clean.py
@@ -2,41 +2,36 @@ import numpy as np
 import pandas as pd
 import json
 import requests
+import ast
 
 from eric_keys import eric_omdb_key, eric_tmdb_key, eric_tmdb_token
 from eric_test import *
 from functions import *
 
 
-##### TMDB Details #####
-
-# Open TMDB Top Rated DF
-df_top = pd.read_csv('TMDB_toprated.csv')
-print(len(df_top))
-print(df_top.keys())
-
-# Drop Adult
-df_top.drop('adult', axis=1, inplace=True)
-
-# Drop Non-English
-lan_filt = df_top['original_language'] != 'en'
-df_top = df_top.loc[lan_filt]
-
-# Write Clean Top Rated DF
-df_top.to_csv('clean_TMDB_toprated.csv', index=False)
-
-# Open Clean Top Rated DF, Indexed
-df_top = pd.read_csv('clean_TMDB_toprated.csv', index_col='title').sort_index()
-
-
-##### TMDB Details #####
-
 # Open TMDB Details DF
-df_det = pd.read_csv('TMDB_movie_details.csv')
-print(len(df_det))
-print(df_det.keys())
+df_det = pd.read_csv('TMDB_movie_details.csv', index_col='id').sort_index()
 
 # Reformat TMDB Details Contries
-import ast
 df_det['countries']=df_det['production_countries'].apply(lambda x: ast.literal_eval(x))
 df_det.drop('production_countries', axis=1, inplace=True)
+df_det.keys()
+
+# Open TMDB Top Rated DF
+df_top = pd.read_csv('TMDB_toprated.csv', index_col='id').sort_index()
+
+# Drop Adult (100% False)
+df_top.drop('adult', axis=1, inplace=True)
+
+# Merge DFs
+combined_df = pd.merge(df_det, df_top, on='id')
+
+# Drop Non-English
+lan_filt = combined_df['original_language'] != 'en'
+combined_df = combined_df.loc[lan_filt]
+
+# Write Merged DF
+combined_df.to_csv('clean_df.csv')
+
+# Open Clean DF
+df = pd.read_csv('clean_df.csv', index_col='id').sort_index()
